273e5512380d6c646402090982725bcb5c753d4b
Fmilena <fmilena1976@gmail.com>
2025-05-02 21:54:41 -0500

    Avanzamos hasta la diapostiva 125

---
+++ Sesión 7/main.py
@@ -0,0 +1,18 @@
+# Escribe esta línea de código
+print("Bienvenidos al entrenamiento con Python, vamos a disfrutar al máximo de esta sesión")
+
+"""
+    Descuento en una compra:
+    Si compras más de $1000 obtienes un descuento del 20%
+    pide el monto de la compra y muestra el precio final
+""" 
+# Pedir datos por teclado al usuario int entero float decimales str cadenas de caracteres bool booleanos
+
+compra = float(input("Por favor digita el valor de la compra: "))
+    
+
+# Si compras más de $1000, obtienes un descuento del 20%
+# Siempre que la salida tenga más de un camino de resolución, debo implementar condicionales
+
+# Operadores combinados
+# Operadores de asignación =, operadores aritméticos +-*/, operadores lógicos and y, or o, not,
